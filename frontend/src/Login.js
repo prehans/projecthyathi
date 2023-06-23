2c6331144cb24d04a4aa3b9671324dc493e2180c
Prehans Gupta <85963909+prehans@users.noreply.github.com>
2023-06-24 03:59:39 +0530

    initial commit

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
-import swal from "sweetalert";
-import { Button, TextField, Link } from "@material-ui/core";
-import { withRouter } from "./utils";
-const axios = require("axios");
-const bcrypt = require("bcryptjs");
+import React, { Component } from 'react';
+import swal from 'sweetalert';
+import { Button, TextField, Link } from '@material-ui/core';
+const axios = require('axios');
+const bcrypt = require('bcryptjs');
 var salt = bcrypt.genSaltSync(10);
 
-class Login extends React.Component {
+export default class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -18,6 +17,7 @@ class Login extends React.Component {
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
   login = () => {
+
     const pwd = bcrypt.hashSync(this.state.password, salt);
 
     axios.post('http://localhost:2000/login', {
@@ -26,8 +26,7 @@ class Login extends React.Component {
     }).then((res) => {
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('user_id', res.data.id);
-      // this.props.history.push('/dashboard');
-      this.props.navigate("/dashboard");
+      this.props.history.push('/dashboard');
     }).catch((err) => {
       if (err.response && err.response.data && err.response.data.errorMessage) {
         swal({
@@ -41,31 +40,13 @@ class Login extends React.Component {
 
   render() {
     return (
-      <div
-        className="login-container"
-        style={{
-          minHeight: "100vh",
-          backgroundImage: "url('https://coolwallpapers.me/picsup/6125673-fantasy-coffee-tsaoshin-yellow-pokemon-pikachu-art-window-cute-cup.jpg')",
-          backgroundSize: "cover",
-          
-          display: "flex",
-          justifyContent: "flex-end",
-          alignItems: "center",
-          padding: "0 50px", // Add padding for spacing
-          
-          
-        }}
-      >
-      
-      {/* <div style={{ marginTop: '50px' }}> */}
-      <div style={{ marginRight: "400px" }}> 
+      <div style={{ marginTop: '200px' }}>
         <div>
-          <h1 >Login</h1>
+          <h2>Login</h2>
         </div>
 
-        <div >
+        <div>
           <TextField
-          color="white"
             id="standard-basic"
             type="text"
             autoComplete="off"
@@ -91,27 +72,17 @@ class Login extends React.Component {
             className="button_style"
             variant="contained"
             color="primary"
-            size="big"
+            size="small"
             disabled={this.state.username == '' && this.state.password == ''}
             onClick={this.login}
           >
             Login
           </Button> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <Link
-            // href="/register"
-            component="button"
-            style={{ fontFamily: "inherit", fontSize: "inherit" }}
-            onClick={() => {
-              this.props.navigate("/register");
-            }}
-          >
+          <Link href="/register">
             Register
           </Link>
         </div>
       </div>
-      </div>
     );
   }
 }
-
-export default withRouter(Login);
